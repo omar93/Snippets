39a99498f5dd38f518ab293ec59049e550bbc581
Omar Alhamad <oa222ct@student.lnu.se>
2020-02-20 01:04:46 +0100

    added a few dependencies for project & app.js working with express

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
-mongoose.connect('mongodb+srv://admin:omar93@1dv023-cspjr.mongodb.net/test?retryWrites=true&w=majority')
+const port = 4000
 
-mongoose.model('users', { name: String })
-
-app.get('/users', (req, res) => {
-  mongoose.model('users').find((er, users) => {
-    res.send(users)
-  })
-})
-
-app.listen('7879')
+app.listen(port, () => console.log(`Server listening on: http://localhost:${port}`))
